37fb9f7a9b1160aae7e11bdfd1c1fe29eddc1a60
danilsawt <nananana189981@gmail.com>
2023-05-26 14:35:30 +0300

    Versionnotrelized

---
+++ src/main/resources/static/JS/admin.js
@@ -0,0 +1,18 @@
+function openForm(){
+  document.getElementById("blablabla").style.display = "block";
+};
+
+function closeForm(){
+  document.getElementById("blablabla").style.display = "none";
+};
+
+function openThemes(){
+  document.getElementById("the").style.display = "block";
+};
+
+function closeThemes(){
+  document.getElementById("the").style.display = "none";
+};
+
+
+
